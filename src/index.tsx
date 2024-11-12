bdba2de9b394e761d36123551d46049f0724db80
bae0203 <gmbae06@dgsw.hs.kr>
2024-11-12 12:14:39 +0900

    add : Main page 추가

--- src/index.tsx
+++ src/index.tsx
@@ -9,9 +9,7 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  //   <ThemeProvider theme={themeColor}>
-  //     <Provider>
-  <div>hi</div>
-  //     </Provider>
-  //   </ThemeProvider>
+  <ThemeProvider theme={themeColor}>
+    <Main />
+  </ThemeProvider>
 );
