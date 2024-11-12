6f796aeb76417ab5220bad2a01a809ed4ba7a73e
bae0203 <gmbae06@dgsw.hs.kr>
2024-11-12 12:09:09 +0900

    init : react 초기 설정

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import Main from "./pages/Main";
+import "./style/index.css";
+// import { ThemeProvider } from "styled-components";
+// import { themeColor } from "./util/style/theme";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  //   <ThemeProvider theme={themeColor}>
+  //     <Provider>
+  <div>hi</div>
+  //     </Provider>
+  //   </ThemeProvider>
+);
