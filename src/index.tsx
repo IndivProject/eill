0059323e73f30c31efb9b083c3902612f2c1faf9
bae0203 <gmbae06@dgsw.hs.kr>
2024-11-13 12:21:27 +0900

    module : reactRouterDom 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Main from "./pages/Main";
+import Detail from "./pages/Detail";
 import "./style/index.css";
 import { ThemeProvider } from "styled-components";
 import { themeColor } from "./style/ThemeColor";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <ThemeProvider theme={themeColor}>
-    <Main />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/detail/:type/:idx" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
   </ThemeProvider>
 );
