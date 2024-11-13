6d762937a745e600d61aa8f31532759413f64cb9
bae0203 <gmbae06@dgsw.hs.kr>
2024-11-13 11:03:11 +0900

    add : 앨범 hover 이벤트 추가

--- src/components/common/Song/index.tsx
+++ src/components/common/Song/index.tsx
@@ -1,22 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import * as S from "./style";
 
 interface IAlbum {
   image: string;
   title: string;
   date: string;
+  inviteSong: string;
   isAlbum?: boolean;
 }
 
-const Song = ({ image, title, date, isAlbum }: IAlbum) => {
+const Song = ({ image, title, date, inviteSong, isAlbum }: IAlbum) => {
+  const [isHover, setIsHover] = useState<boolean>(false);
+
   return (
-    <div>
-      <img src="" alt="" />
-      <div>
-        <p></p>
-        <p></p>
-        <p>{isAlbum && "..."}</p>
-      </div>
-    </div>
+    <S.MainWrap>
+      <S.ImageWrap
+        onMouseOver={() => setIsHover(true)}
+        onMouseOut={() => setIsHover(false)}
+      >
+        {
+          <S.ImageFilter isHover={isHover}>
+            <S.DetailContext>자세히 보기</S.DetailContext>
+          </S.ImageFilter>
+        }
+        <S.Image src={image} alt="image" />
+      </S.ImageWrap>
+      <S.ContextWrap>
+        <S.Context>{title}</S.Context>
+        <S.Context>{date}</S.Context>
+        <S.Context>
+          {inviteSong}
+          {isAlbum && "..."}
+        </S.Context>
+      </S.ContextWrap>
+    </S.MainWrap>
   );
 };
 
--- src/components/common/Song/style.ts
+++ src/components/common/Song/style.ts
@@ -1,3 +1,59 @@
 import styled from "styled-components";
 
-export const MainWrap = styled.div``;
+export const MainWrap = styled.div`
+  margin-right: 2rem;
+  display: flex;
+  width: 500px;
+`;
+
+export const ImageWrap = styled.div`
+  border-radius: 0.5rem;
+  width: 12rem;
+  height: 12rem;
+  overflow: hidden;
+  position: relative;
+
+  cursor: pointer;
+`;
+
+export const Image = styled.img`
+  background-color: ${({ theme }) => theme.personal};
+  width: 12rem;
+  height: 12rem;
+`;
+
+export const ImageFilter = styled.div<{ isHover: boolean }>`
+  background-color: rgba(0, 0, 0, 0.3);
+  position: absolute;
+  width: 12rem;
+  height: 12rem;
+
+  transition: 0.5s;
+  margin-top: ${({ isHover }) => (isHover ? "0rem" : "12rem")};
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const DetailContext = styled.p`
+  font-size: 1rem;
+  font-family: "Bold";
+
+  color: ${({ theme }) => theme.white10};
+`;
+
+export const ContextWrap = styled.div`
+  margin-left: 2rem;
+`;
+
+export const Context = styled.p`
+  font-size: 1rem;
+  margin-bottom: 0.25rem;
+  ${({ theme }) => theme.personal10};
+
+  &:nth-child(1) {
+    font-family: "Medium";
+    font-size: 1.5rem;
+  }
+`;
