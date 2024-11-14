58eef26f1160f674c30abc1f2c2c22d7f9503ae0
bae0203 <gmbae06@dgsw.hs.kr>
2024-11-14 14:31:55 +0900

    add : Single 페이지

--- src/pages/SingleDetail/index.tsx
+++ src/pages/SingleDetail/index.tsx
@@ -1,43 +1,74 @@
-import React, { useState } from "react";
-import { useParams } from "react-router-dom";
-import { Album } from "../../constant/Album";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import { Single } from "../../constant/Single";
-import { IAlbum, ISingle } from "../../constant/type";
+import { ISingle } from "../../constant/type";
 import Button from "../../components/common/Button";
+import YouTube from "react-youtube";
+import * as S from "./style";
 
 const SingleDetail = () => {
+  const navigate = useNavigate();
   const params = useParams();
-
-  const AlbumInfo: IAlbum = Album[Number(params.idx)];
   const SingleInfo: ISingle = Single[Number(params.idx)];
 
-  const [index, setIndex] = useState<number>(0);
+  const [isPlay, setIsPlay] = useState<boolean>(false);
+  const setPlayState = (val: boolean) => setIsPlay(val);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
-    <>
-      <div>
-        <p>{params.type == "1" ? AlbumInfo.title : SingleInfo.title}</p>
-        <Button context="돌아가기" onClick={() => {}} />
-      </div>
-      <img
-        src={params.type == "1" ? AlbumInfo.image : SingleInfo.image}
-        alt="image"
-      />
-      <p>
-        {params.type == "1"
-          ? AlbumInfo.songs.map((e, i) => <p key={i}>{e}</p>)
-          : SingleInfo.title}
-      </p>
-      <p>{params.type == "1" ? AlbumInfo.date : SingleInfo.date}</p>
-      <p>
-        {SingleInfo.lyrics ? (
-          SingleInfo.lyrics.split("=").map((e) => <p>{e == "" ? "ㅤ" : e}</p>)
-        ) : (
-          <p>가사 정보 없음</p>
-        )}
-      </p>
-      <Button context="들으러 가기" onClick={() => {}} />
-    </>
+    <S.MainWrap>
+      <S.SongWrap>
+        <S.AlbumButtonWrap>
+          <S.AlbumWrap>
+            <S.AlbumCover src={SingleInfo.image} alt="" />
+            <S.AlbumCenter />
+            <S.SongImg isPlay={isPlay} src={SingleInfo.image} alt="image" />
+            <S.AlbumBackground src={SingleInfo.image} />
+          </S.AlbumWrap>
+          <Button context="돌아가기" onClick={() => navigate("/")} />
+        </S.AlbumButtonWrap>
+        <div>
+          <S.Title>{SingleInfo.title}</S.Title>
+          <S.Line />
+          <S.Lyrics>{SingleInfo.date}</S.Lyrics>
+
+          <S.SubTitle>가사</S.SubTitle>
+          <p>
+            {SingleInfo.lyrics ? (
+              SingleInfo.lyrics
+                .split("=")
+                .map((e) => <S.Lyrics>{e == "" ? "ㅤ" : e}</S.Lyrics>)
+            ) : (
+              <S.Lyrics>가사 정보 없음</S.Lyrics>
+            )}
+          </p>
+        </div>
+        <S.YoutubeBoxWrap>
+          <S.YoutubeBox>
+            <YouTube
+              videoId={SingleInfo.url}
+              opts={{
+                width: "560",
+                height: "315",
+                playerVars: {
+                  autoplay: 0,
+                  rel: 0,
+                  modestbranding: 1,
+                },
+              }}
+              onPlay={() => setPlayState(true)}
+              onPause={() => setPlayState(false)}
+              onEnd={(e: any) => {
+                e.target.stopVideo(0);
+              }}
+            />
+          </S.YoutubeBox>
+        </S.YoutubeBoxWrap>
+      </S.SongWrap>
+    </S.MainWrap>
   );
 };
 
---
+++ src/pages/SingleDetail/style.ts
@@ -0,0 +1,111 @@
+import styled, { keyframes } from "styled-components";
+
+const SpinAnime = keyframes`
+    100%{transform:rotate(360deg);}
+`;
+
+export const SongWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  width: 80%;
+  margin: 0 auto;
+`;
+
+export const SongImg = styled.img<{ isPlay: boolean }>`
+  border-radius: 200px;
+  width: 350px;
+  height: 350px;
+  z-index: 1;
+
+  transform-origin: 50% 50%;
+  animation: ${SpinAnime} 10s linear infinite;
+  ${({ isPlay }) => !isPlay && `animation-play-state: paused;`}
+  border: 1px solid ${({ theme }) => theme.personal10};
+
+  box-shadow: 0rem 0rem 1rem rgba(0, 0, 0, 0.5);
+`;
+
+export const AlbumWrap = styled.div`
+  width: 400px;
+  height: 400px;
+  box-sizing: boerder-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 3rem;
+`;
+
+export const AlbumBackground = styled.img`
+  position: absolute;
+  width: 400px;
+  height: 400px;
+  filter: blur(4px);
+  z-index: 0;
+`;
+
+export const AlbumCover = styled.img`
+  position: absolute;
+  z-index: 3;
+  width: 420px;
+  height: 420px;
+  clip-path: polygon(0% 0%, 100% 0%, 100% 50%, 50% 50%, 50% 100%, 0% 100%);
+  border: 1px solid ${({ theme }) => theme.personal30};
+`;
+
+export const AlbumCenter = styled.div`
+  position: absolute;
+  z-index: 2;
+  width: 50px;
+  height: 50px;
+  border-radius: 50px;
+
+  background-color: ${({ theme }) => theme.white30};
+  border: 2px solid ${({ theme }) => theme.personal10};
+`;
+
+export const AlbumButtonWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: right;
+`;
+
+export const Title = styled.p`
+  font-size: 3rem;
+  font-family: "Bold";
+`;
+
+export const SubTitle = styled.p`
+  font-size: 1.75rem;
+  font-family: "Bold";
+  margin-top: 3rem;
+`;
+
+export const Lyrics = styled.p`
+  font-size: 1rem;
+  margin-top: 0.5rem;
+  color: ${({ theme }) => theme.white30};
+`;
+
+export const YoutubeBox = styled.div`
+  position: fixed;
+  background-color: gray;
+`;
+
+export const YoutubeBoxWrap = styled.div`
+  position: relative;
+  width: 30%;
+`;
+
+export const Line = styled.div`
+  width: 100%;
+  height: 2px;
+  border-radius: 1rem;
+  margin: 1rem 0;
+  background-color: ${({ theme }) => theme.personal};
+`;
+
+export const MainWrap = styled.div`
+  background-color: ${({ theme }) => theme.white10};
+  padding: 10rem 0;
+`;
